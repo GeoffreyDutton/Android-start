1774c36b209501162a41f4a138c13e31f6952065
Geoff Dutton <geoffrey.dutton@outlook.com>
2017-02-15 16:41:51 -0500

    Checkpoint-32-createproject completed

---
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,19 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
+
+/**
+ * Created by Geoff on 2/15/2017.
+ */
+
+public class BloclyActivity extends Activity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+
+    }
+}
