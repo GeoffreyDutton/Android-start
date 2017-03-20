5990093e0562cd3b52c0a79193351a1fd9851544
Geoffrey Dutton <geoffrey.dutton@outlook.com>
2017-03-20 19:01:59 -0400

    Checkpoint 51 Android databases pt1: completed

---
+++ app/src/main/java/io/bloc/android/blocly/api/model/database/table/table.java
@@ -0,0 +1,17 @@
+package io.bloc.android.blocly.api.model.database.table;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Geoff on 3/20/2017.
+ */
+
+public abstract class Table {
+    protected static final String COLUMN_ID = "id";
+    public abstract String getName();
+    public abstract String getCreateStatement();
+
+    public void onUpgrade(SQLiteDatabase writableDatabse, int oldVersion, int newVersion ){
+
+    }
+}
